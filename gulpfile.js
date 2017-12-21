cb79600946f75c7efafc61b527e45f5e40039f8e
华清峰 <80506313@qq.com>
2017-12-21 13:35:10 +0800

    完成环境配置

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,23 @@ const exec = require('child_process').exec;
 
 const cmd = `./node_modules/.bin/`;
 const appName = 'api';
-console.log(process.env.npm_lifecycle_event)
+
+let pm2_args = '--watch --source-map-support';
+let env = process.env.NODE_ENV || 'dev';
+
+// 设置服务器 log 目录
+if(process.env.npm_lifecycle_event.indexOf('run') >= 0) {
+  pm2_args += ` --log-date-format=\"YYYY-MM-DD HH:mm:ss.SSS\" -i 1 -o /opt/tuniu/logs/app/nodejs/${appName}-out.log -e /opt/tuniu/logs/app/nodejs/${appName}-error.log`;
+  env = process.env.npm_lifecycle_event.split(':')[1];
+}
+const pm2Cmd = `${cmd}pm2 start ./bin/pm2.${env}.config.js --name "${appName}" ${pm2_args}`
+
 gulp.task('tsc', shell.task([
   `tsc`
 ]));
-gulp.task('pm2-start', ['tsc'], shell.task([
-  `${cmd}pm2 start ./process.config.js --watch --source-map-support`
-]));
+gulp.task('pm2-start', ['tsc'], () => {
+  exec(pm2Cmd)
+});
 gulp.task('pm2-delete', () => {
   exec(`pm2 delete ${appName}`);
 })
