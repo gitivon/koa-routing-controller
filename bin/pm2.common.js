7eb275601326482a0d3242dfc8bf020fa337838d
华清峰 <80506313@qq.com>
2017-12-21 19:29:53 +0800

    no message

--- bin/pm2.common.js
+++ bin/pm2.common.js
@@ -11,7 +11,9 @@ module.exports = {
       "env": {
         "NODE_ENV": "development"
       },
-      "log_date_format": "YYYY-MM-DD HH:mm:ss"
+      "log_date_format": "YYYY-MM-DD HH:mm:ss",
+      "out_file": "/opt/tuniu/logs/app/api-out.log",
+      "error_file": "/opt/tuniu/logs/app/api-error.log"
     }
   ]
 }
